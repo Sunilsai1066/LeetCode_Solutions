9a12f5ac2fd779e45e166195f74f5c1774689805
Sunilsai1066 <sunilsai6268@gmail.com>
2022-02-02 22:40:12 +0530

    Kth Factor

---
+++ 1492. The kth Factor of n.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        for i in range(1,n//2 + 1):
+            if(n%i == 0):
+                k -= 1
+                if(k == 0):
+                    return i
+        if(k == 1):
+            return n
+        return -1
