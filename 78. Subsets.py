a443686a689bff5f28e26ff462afb4521f550372
Sunilsai1066 <sunilsai6268@gmail.com>
2022-03-19 14:56:14 +0530

    Subsets Recursion Added

--- 78. Subsets.py
+++ 78. Subsets.py
@@ -1,3 +1,20 @@
+#Recursion Solution
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        FinRes = []
+        Start,End = 0,len(nums)-1
+        def subSets(Lis,Res,Start,End,FinRes):
+            if(Start > End):
+                FinRes.append(Res[:])
+                return
+            Res.append(Lis[Start])
+            subSets(Lis,Res,Start+1,End,FinRes)
+            Res.pop()
+            subSets(Lis,Res,Start+1,End,FinRes)
+        subSets(nums,[],Start,End,FinRes)
+        return FinRes
+
+#Power Set Method
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         Res = []
